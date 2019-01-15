e2ac504a61027d048bf7e7f02498e4b0a678cbed
Xenossolitarius <ipesic@fesb.hr>
2019-01-15 11:29:56 +0100

    Created files

---
+++ src/Components/Autocomplete.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class Autocomplete extends Component {
+  render() {
+    return (
+      <div className="Autocomplete">
+        Autocomplete
+      </div>
+    );
+  }
+}
+
+export default Autocomplete;
