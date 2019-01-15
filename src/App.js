b813e0a1e3f7c854cc3cdb51b7531a0f947af3f3
Xenossolitarius <ipesic@fesb.hr>
2019-01-15 11:50:19 +0100

    Added search data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Autocomplete from './Components/Autocomplete'
+import countriesJsonObj from './input/inputDataJson'
+import countriesJsonString from './input/inputDataString'
 
 class App extends Component {
   render() {
